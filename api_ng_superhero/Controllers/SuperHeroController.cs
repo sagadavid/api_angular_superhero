0ff5f7b960ab95f87631882924e17ecd24a732c2
sagadavid <epostsaga@gmail.com>
2023-06-16 01:20:48 +0200

    Add project files.

---
+++ api_ng_superhero/Controllers/SuperHeroController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_ng_superhero.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
+        {
+            return new List<SuperHero>
+        { new SuperHero {
+        Nick="edderkopp man",
+        FirstName="dave",
+        LastName="saga",
+        Place="agder"
+        }
+        };
+        }
+    }
+}
